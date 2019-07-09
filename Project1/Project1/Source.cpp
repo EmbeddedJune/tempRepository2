054ce6cbdb01128469bba62ba38a932868e14a8d
EmbeddedJune <cydphj@gmail.com>
2019-07-09 11:51:33 +0900

    include double

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -11,4 +11,13 @@ int main()
 	for (unsigned i = 0; i < 5; ++i)
 		std::cout << a[i] << " ";
 	std::cout << "\n";
+
+	double* b = new double[5];
+	for (unsigned i = 0; i < 5; ++i)
+		b[i] = i * 3.14;
+
+	std::cout << " b = ";
+	for (unsigned i = 0; i < 5; ++i)
+		std::cout << b[i] << " ";
+	std::cout << "\n";
 }
